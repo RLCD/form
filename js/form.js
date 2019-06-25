f2bf1ed9c20a7388d7883c25f2e66014ea8caaa4
Kyle Schwarz <zeranoe@gmail.com>
2019-06-24 23:23:24 -0400

    Start building selects from an items var

--- js/form.js
+++ js/form.js
@@ -1,3 +1,9 @@
-$("form").submit(function(e) {
-    console.log("external script file text");
-});
+function fill_dropdowns() {
+    var options = "";
+    for (var i = 0; i < items.bodies.length; i++) {
+        options += "<option>" + items.bodies[i].name + "</option>";
+    }
+    $("#body").html(options);
+}
+
+fill_dropdowns();
---
+++ js/items.js
@@ -0,0 +1,18 @@
+var items = {
+    bodies: [
+        {
+            name: "Octane",
+            paintable: true
+        },
+        {
+            name: "Merc",
+            paintable: true
+        }
+    ],
+    wheels: [
+        {
+            name: "Zomba",
+            paintable: true
+        }
+    ]
+};
