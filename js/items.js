f485235fd4d272983cee8cb210552b768f3d2872
Kyle Schwarz <zeranoe@gmail.com>
2019-06-25 02:36:32 -0400

    Wrote a bat script to generate items.js, first test

--- js/items.js
+++ js/items.js
@@ -1,18 +1,272 @@
 var items = {
     bodies: [
         {
-            name: "Octane",
+            name: "'16 Batmobile",
+            paintable: false
+        },
+        {
+            name: "'70 Dodge Charger R/T",
+            paintable: false
+        },
+        {
+            name: "'89 Batmobile",
+            paintable: false
+        },
+        {
+            name: "'99 Nissan Skyline GT-R R34",
+            paintable: false
+        },
+        {
+            name: "Aftershock",
+            paintable: false
+        },
+        {
+            name: "Animus GP",
             paintable: true
+        },
+        {
+            name: "Artemis G1",
+            paintable: false
+        },
+        {
+            name: "Artemis GXT",
+            paintable: true
+        },
+        {
+            name: "Artemis",
+            paintable: false
+        },
+        {
+            name: "Backfire",
+            paintable: false
+        },
+        {
+            name: "Bone Shaker",
+            paintable: false
+        },
+        {
+            name: "Breakout Type-S",
+            paintable: false
+        },
+        {
+            name: "Breakout",
+            paintable: true
+        },
+        {
+            name: "Centio V17",
+            paintable: true
+        },
+        {
+            name: "Cyclone",
+            paintable: true
+        },
+        {
+            name: "DeLorean Time Machine",
+            paintable: false
+        },
+        {
+            name: "Diestro",
+            paintable: true
+        },
+        {
+            name: "Dominus GT",
+            paintable: false
+        },
+        {
+            name: "Dominus",
+            paintable: false
+        },
+        {
+            name: "Ecto-1",
+            paintable: false
+        },
+        {
+            name: "Endo",
+            paintable: true
+        },
+        {
+            name: "Esper",
+            paintable: false
+        },
+        {
+            name: "Fast 4WD",
+            paintable: false
+        },
+        {
+            name: "Fennec",
+            paintable: true
+        },
+        {
+            name: "Gazella GT",
+            paintable: false
+        },
+        {
+            name: "Gizmo",
+            paintable: false
+        },
+        {
+            name: "Grog",
+            paintable: false
+        },
+        {
+            name: "Guardian G1",
+            paintable: false
+        },
+        {
+            name: "Guardian GXT",
+            paintable: true
+        },
+        {
+            name: "Guardian",
+            paintable: false
+        },
+        {
+            name: "Hotshot",
+            paintable: false
+        },
+        {
+            name: "Ice Charger",
+            paintable: false
+        },
+        {
+            name: "Imperator DT5",
+            paintable: true
+        },
+        {
+            name: "J„ger 619 RS",
+            paintable: true
+        },
+        {
+            name: "Jurassic Jeep? Wrangler",
+            paintable: false
+        },
+        {
+            name: "K.I.T.T.",
+            paintable: false
+        },
+        {
+            name: "Mantis",
+            paintable: true
+        },
+        {
+            name: "Marauder",
+            paintable: false
+        },
+        {
+            name: "Masamune",
+            paintable: false
+        },
+        {
+            name: "Maverick G1",
+            paintable: false
+        },
+        {
+            name: "Maverick GXT",
+            paintable: true
+        },
+        {
+            name: "Maverick",
+            paintable: false
+        },
+        {
+            name: "McLaren 570S",
+            paintable: false
         },
         {
             name: "Merc",
             paintable: true
-        }
-    ],
-    wheels: [
+        },
         {
-            name: "Zomba",
+            name: "MR11",
+            paintable: false
+        },
+        {
+            name: "Nimbus",
             paintable: true
-        }
-    ]
+        },
+        {
+            name: "Octane ZSR",
+            paintable: false
+        },
+        {
+            name: "Octane",
+            paintable: true
+        },
+        {
+            name: "Paladin",
+            paintable: false
+        },
+        {
+            name: "Proteus",
+            paintable: false
+        },
+        {
+            name: "Ripper",
+            paintable: false
+        },
+        {
+            name: "Road Hog XL",
+            paintable: false
+        },
+        {
+            name: "Road Hog",
+            paintable: true
+        },
+        {
+            name: "Samurai",
+            paintable: true
+        },
+        {
+            name: "Scarab",
+            paintable: false
+        },
+        {
+            name: "Takumi RX-T",
+            paintable: false
+        },
+        {
+            name: "Takumi",
+            paintable: false
+        },
+        {
+            name: "The Dark Knight Rises Tumbler",
+            paintable: false
+        },
+        {
+            name: "Triton",
+            paintable: false
+        },
+        {
+            name: "Twin Mill III",
+            paintable: false
+        },
+        {
+            name: "Twinzer",
+            paintable: true
+        },
+        {
+            name: "Venom",
+            paintable: true
+        },
+        {
+            name: "Vulcan",
+            paintable: false
+        },
+        {
+            name: "Werewolf",
+            paintable: true
+        },
+        {
+            name: "X-Devil Mk2",
+            paintable: false
+        },
+        {
+            name: "X-Devil",
+            paintable: true
+        },
+        {
+            name: "Zippy",
+            paintable: false
+        },
+    ],
 };
