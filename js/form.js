c8e2bd4349eb5c080feb29eb324c7a0c56389ac3
Kyle Schwarz <zeranoe@gmail.com>
2019-06-26 10:31:03 -0400

    Initial tab design

--- js/form.js
+++ js/form.js
@@ -1,18 +1,22 @@
 function fill_dropdown(slot) {
-    var options = "";
+    var options = '';
     for (var i = 0; i < items[slot].length; i++) {
-        options += "<option>" + items[slot][i].name + "</option>";
+        options += '<option>' + items[slot][i].name + '</option>';
     }
-    $("#" + slot).html(options);
+    $('#' + slot).html(options);
 }
 
 function fill_dropdowns() {
-    fill_dropdown("bodies");
-    fill_dropdown("decals");
-    fill_dropdown("wheels");
-    fill_dropdown("boosts");
-    fill_dropdown("toppers");
+    fill_dropdown('bodies');
+    fill_dropdown('decals');
+    fill_dropdown('wheels');
+    fill_dropdown('boosts');
+    fill_dropdown('toppers');
 }
 
+$('#tabs a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
 
 fill_dropdowns();
