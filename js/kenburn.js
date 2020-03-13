06d5d60cea932b2bae37fdbd487dcd94b885e454
vikasbhatia <vikasbhatia11@gmail.com>
2020-03-13 16:39:57 +0530

    Add maintenance page

---
+++ js/kenburn.js
@@ -0,0 +1,10 @@
+$(".slideshow-bg main.background").vegas({
+	delay: 9000,
+    slides: [
+        { src: 'images/bg-1.jpg' },
+        { src: 'images/bg-2.jpg' },
+        { src: 'images/bg-3.jpg' },
+        { src: 'images/bg-4.jpg' }
+    ],
+    animation: 'kenburns'
+});
